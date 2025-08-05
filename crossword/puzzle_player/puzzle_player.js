e73b62abba180c166689c79b2dc39f83ced7975e
L Denney <156242030+loladenney@users.noreply.github.com>
2025-08-05 09:33:01 -0700

    adding a favicon

--- crossword/puzzle_player/puzzle_player.js
+++ crossword/puzzle_player/puzzle_player.js
@@ -1,2 +1,7 @@
 'use strict';
 
+//read from some puzzlejson file
+// build up the player from it
+// then add all the interactivity, inputing, autochecking when grid is complete, reveal answer
+// add a letter check, reveal answer
+JSON.parse()
