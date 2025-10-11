1acd410c863adde9f04f203ba3cd701395b54ebe
Lola Denney <lolacdenney@gmail.com>
2025-10-11 15:25:52 -0700

    favourites

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ function isMouseOverDragonHead(event) {
 // Change the image on mouse move (while the mouse is over the image)
 image.addEventListener('mousemove', function(event) {
     if (isMouseOverDragonHead(event)) {
-        image.src = 'assets/dragon_header_eating.png';  // Change to the hover image
-        image.style.cursor = 'url(assets/scared_cursor.png), auto';
+        image.src = 'https://shadowthehedgehog.ca/assets/dragon_header_eating.png';  // Change to the hover image
+        image.style.cursor = 'url(https://shadowthehedgehog.ca/assets/scared_cursor.png), auto';
     } else {
-        image.src = 'assets/dragon_header.png';  // Reset to the original image when the mouse moves away
-        image.style.cursor = 'url(assets/neutral_cursor.png), auto';
+        image.src = 'https://shadowthehedgehog.ca/assets/dragon_header.png';  // Reset to the original image when the mouse moves away
+        image.style.cursor = 'url(https://shadowthehedgehog.ca/assets/neutral_cursor.png), auto';
     }
 });
 
 image.addEventListener('mouseleave', function() {
-    image.src = 'assets/dragon_header.png';  // Revert to the original image when the mouse leaves the image
-    image.style.cursor = 'url(assets/neutral_cursor.png), auto';
+    image.src = 'https://shadowthehedgehog.ca/assets/dragon_header.png';  // Revert to the original image when the mouse leaves the image
+    image.style.cursor = 'url(https://shadowthehedgehog.ca/assets/neutral_cursor.png), auto';
 });
